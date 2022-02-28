7f15d7551c5c65be154cec4538cb3f9586959fe7
shadikmahamudsohan <shadikmahamudsohan@gmail.com>
2022-02-28 22:11:52 +0600

    search section added, getting  phone input form api

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('testing');
+const loadPhoneCards = () => {
+    const inputValue = document.getElementById("search-input").value;
+    const inputValueLowerCase = inputValue.toLowerCase();
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValueLowerCase}`
+
+    // fetching the url
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
