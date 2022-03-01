2e655f758752f5d1cf6b224e787d1e9577f26642
shadikmahamudsohan <shadikmahamudsohan@gmail.com>
2022-03-01 21:09:02 +0600

    adding brand in card and improved code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// arrow function for loading data from api
 const loadProductData = () => {
     const input = document.getElementById("search-input");
     const inputValue = input.value;
@@ -12,26 +13,32 @@ const loadProductData = () => {
 
 }
 
+// arrow function for displaying data inside html
 const displayProductCard = (data) => {
+    const cardContainer = document.getElementById('card-container')
+    const cardDetails = document.getElementById('card-details');
     const inputError = document.getElementById('input-error')
+    // clearing cards and card details
+    cardDetails.textContent = "";
+    cardContainer.textContent = "";
     if (data.status == false) {
         inputError.classList.remove('d-none')
     } else {
         inputError.classList.add('d-none')
         const products = data.data.slice(0, 20);
-        const cardContainer = document.getElementById('card-container')
-        const cardDetails = document.getElementById('card-details');
-        cardDetails.textContent = "";
-        cardContainer.textContent = "";
+
         products.forEach(product => {
             const div = document.createElement('div');
             div.className = "col-lg-4"
+
+            // showing cards
             div.innerHTML = `
                     <div class="card h-100 shadow">
                         <img src="${product.image}"
                             class="card-img-top p-4 img-fluid" alt="image">
                         <div class="card-body">
-                            <h4 class="card-title">${product.phone_name}</h4>
+                            <h2 class="card-title">${product.phone_name}</h2>
+                            <h4>Brand: ${product.brand}</h4>
                             <p class="card-text"></p>
                             <button onclick="loadCardDetail('${product.slug}')" class="btn btn-primary px-4 py-2">Details</button>
                         </div>
@@ -42,6 +49,7 @@ const displayProductCard = (data) => {
     }
 }
 
+// arrow function for loading details
 const loadCardDetail = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     // fetching detail api
@@ -50,13 +58,15 @@ const loadCardDetail = (id) => {
         .then(data => displayCardDetail(data))
 }
 
+// arrow function for displaying details
 const displayCardDetail = (data) => {
     console.log(data);
     const productDetail = data.data;
     const cardDetails = document.getElementById('card-details');
     const mainFeatures = Object.entries(productDetail.mainFeatures);
-    const otherFeatures = Object.entries(productDetail.others ? productDetail.others : ['not available']);
+    const otherFeatures = Object.entries(productDetail.others ? productDetail.others : ['Not available']);
 
+    // showing card details
     cardDetails.innerHTML = `
             <div  class="row p-5 bg-white shadow my-5">
                 <div class="col-lg-6">
@@ -65,7 +75,8 @@ const displayCardDetail = (data) => {
                 </div>
                 <div class="col-lg-6 border-start border-dark border-3">
                     <h1>${productDetail.name}</h1>
-                    <p class="text-danger">${productDetail.releaseDate ? productDetail.releaseDate : 'not available'}</p>
+                    <h4>Brand: ${productDetail.brand}</h4>
+                    <p class="text-danger">${productDetail.releaseDate ? productDetail.releaseDate : 'Not release date found'}</p>
                     <h4 class="mt-4"><u>Main features</u></h4>
                     <p id="features"></p>
                     <p id="sensors"></p>
@@ -77,6 +88,7 @@ const displayCardDetail = (data) => {
     // adding features inside card details
     const mainFeaturesId = document.getElementById('features');
     const sensorsId = document.getElementById('sensors');
+    // looping trough all the main features
     mainFeatures.forEach(feature => {
         const p = document.createElement('p')
         p.innerHTML = `<strong>${feature[0]}:</strong> ${feature[1]}.`;
@@ -94,6 +106,8 @@ const displayCardDetail = (data) => {
     })
     // adding other features inside card details
     const otherFeaturesId = document.getElementById('other-features');
+
+    // looping trough all the other features
     otherFeatures.forEach(feature => {
         const p = document.createElement('p')
         p.innerHTML = `<strong>${feature[0]}:</strong> ${feature[1]}.`;
